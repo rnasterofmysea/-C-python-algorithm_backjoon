4b432717042ee8af79b363b07ccefc306b1139d8
rnasterofmysea <81907470+rnasterofmysea@users.noreply.github.com>
2024-01-25 21:43:29 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+def Fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n ==2:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+print(Fibonacci(n))
