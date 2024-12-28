c54c65d1634940a853a1469454dd4519c993a104
DaeHyun Kim <81907470+rnasterofmysea@users.noreply.github.com>
2024-12-28 23:47:36 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 5792 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.c
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.c
@@ -1,47 +1,50 @@
-#include <stdio.h>
-#include <string.h>
+/*
+전체 크기 N
 
-#define MAX 2187
+기저조건
+N이 3일 경우 해당 모양 출력
+
+0 ~ N /3 > 줄어든 그대로 출력
+
+N /3 ~ N * 2 / 3 > 중간이 비어야함
+
+N / 3 ~ N > 줄어든 그대로 출력
+*/
+
+#include <stdio.h>
+#include <string.h> // memset 사용을 위해 추가
+
+#define MAX 2188
 char arry[MAX][MAX];
 
-// 재귀적으로 별 찍기 함수
-void recursion(int x, int y, int len) {
-    // 기저 조건
-    if (len == 1) {
-        arry[y][x] = '*';
+void recursion(int x, int y, int len){
+    //기저 조건
+    if(len == 1){
+        arry[x][y] = '*';
         return;
     }
-
-    int new_len = len / 3;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            // 중간 부분은 비워둠
-            if (i == 1 && j == 1) {
+    int new_len = len/3;
+    for(int i = 0; i < 3 ; i ++){
+        for(int j = 0; j < 3; j ++){
+            if(i == 1 && j == 1){
                 continue;
+            } else{
+                recursion(x + j * new_len, y + i * new_len, new_len);
             }
-            // 나머지 부분에 대해 재귀 호출
-            recursion(x + j * new_len, y + i * new_len, new_len);
         }
     }
-}
+    
+};
 
 int main() {
     int N = 0;
     scanf("%d", &N);
-
-    // 배열 초기화 (공백으로 채움)
     memset(arry, ' ', sizeof(arry));
+    recursion(0,0,N);
 
-    // 재귀 호출로 별 찍기
-    recursion(0, 0, N);
-
-    // 출력
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            putchar(arry[i][j]);
-        }
-        putchar('\n');
+    for(int i = 0 ; i < N; i++){
+        arry[i][N] = '\0'; // 각 행의 끝에 null 문자 추가
+        printf("%s\n", arry[i]);
     }
-
     return 0;
 }
