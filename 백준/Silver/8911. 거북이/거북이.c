75875ebd08c6225b72556147294a9f9f8d3a2547
DaeHyun Kim <81907470+rnasterofmysea@users.noreply.github.com>
2025-01-09 14:05:08 +0900

    [Silver III] Title: 거북이, Time: 40 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.c
+++ 백준/Silver/8911. 거북이/거북이.c
@@ -1,68 +1,55 @@
 #include <stdio.h>
 #include <string.h>
-/*
-{{0,-1},{1,0},{0,1},{-1,0}}
 
-0,-1 > 왼쪽으로 회전 시 ?-1,0 > 한번더 > 0, 1 > 1, 0 > 0, -1
-     > 오른쪽 회전 시  > 1, 0 >         1,0 >  0,1 > -1,0 
-*/
+#define MAX_LEN 500
 
-int N = 0;
+// 방향: 북(0), 동(1), 남(2), 서(3)
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+void solve() {
+    int n;
+    char commands[MAX_LEN + 1];
+    scanf("%d", &n);
+
+    while (n--) {
+        scanf("%s", commands);
+        
+        int x = 0, y = 0; // 현재 좌표
+        int dir = 0;      // 현재 방향 (북쪽)
+
+        int min_x = 0, max_x = 0;
+        int min_y = 0, max_y = 0;
+
+        int len = strlen(commands);
+        for (int i = 0; i < len; i++) {
+            char cmd = commands[i];
+            if (cmd == 'F') { // 앞으로 이동
+                x += dx[dir];
+                y += dy[dir];
+            } else if (cmd == 'B') { // 뒤로 이동
+                x -= dx[dir];
+                y -= dy[dir];
+            } else if (cmd == 'L') { // 왼쪽 회전
+                dir = (dir + 3) % 4;
+            } else if (cmd == 'R') { // 오른쪽 회전
+                dir = (dir + 1) % 4;
+            }
+
+            // 방문 좌표의 최소/최대 갱신
+            if (x < min_x) min_x = x;
+            if (x > max_x) max_x = x;
+            if (y < min_y) min_y = y;
+            if (y > max_y) max_y = y;
+        }
+
+        // 직사각형 넓이 계산
+        int area = (max_x - min_x) * (max_y - min_y);
+        printf("%d\n", area);
+    }
+}
 
 int main() {
-
-    int direction[4][2] = {{0,-1},{1,0},{0,1},{-1,0}};
-    scanf("%d", &N);
-    
-    for(int i = 0; i < N; i ++){
-        int current[2] = {0,0};
-        int current_dir = 0;
-        char prompt[501];
-        int history[500][2];
-        int move_count = 0;
-        scanf("%s", prompt);
-        int len = strlen(prompt);
-        for(int j = 0; j < len; j ++){
-            if(prompt[j] == 'L'){
-                current_dir -= 1;
-                if(current_dir <0){
-                    current_dir += 4;
-                }
-            }else if(prompt[j] == 'R'){
-                current_dir +=1;
-                if(current_dir > 3){
-                    current_dir -= 4;
-                }
-            }else if(prompt[j] == 'F'){
-                current[0] = current[0] + direction[current_dir][0];
-                current[1] = current[1] + direction[current_dir][1];
-                history[move_count][0] = current[0];
-                history[move_count][1] = current[1];
-                move_count ++;
-            } else if(prompt[j] == 'B'){
-                current[0] = current[0] + direction[(current_dir + 2) % 4][0];
-                current[1] = current[1] + direction[(current_dir + 2) % 4][1];
-                history[move_count][0] = current[0];
-                history[move_count][1] = current[1];
-                move_count ++;
-            }
-        }
-        int min_x = 0, min_y = 0, max_x = 0 , max_y = 0;
-
-        for(int x = 0 ; x < move_count; x ++){
-            if(history[x][0] > 0 && history[x][0] > max_x){
-                max_x = history[x][0];
-            } else if(history[x][0] < 0 && history[x][0] < min_x){
-                min_x = history[x][0];
-            }
-
-            if(history[x][1] > 0 && history[x][1] > max_y){
-                max_y = history[x][1];
-            } else if(history[x][1] < 0 && history[x][1] < min_y){
-                min_y = history[x][1];
-            }
-        }
-        int result = (max_x - min_x) * (max_y - min_y);
-        printf("%d\n", result);        
-    }
+    solve();
+    return 0;
 }
