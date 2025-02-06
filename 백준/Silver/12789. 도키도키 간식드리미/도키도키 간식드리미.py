37bdaf23f35166cc68c83715722abdb784c582cf
DaeHyun Kim <81907470+rnasterofmysea@users.noreply.github.com>
2025-02-06 15:11:25 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -2,24 +2,29 @@ N = int(input())
 arry = list(map(int, input().split()))
 start = 0
 stack = []
-for i in range(1,N):
+
+for i in range(1, N):
     target = i
-    temp = 0
+    found = False
+
+    # 배열에서 target 찾기
     for j in range(start, N):
-        if target != arry[j] :
+        if target != arry[j]:
             stack.append(arry[j])
         else:
-            start = j+1
-            temp = 1
+            start = j + 1
+            found = True
             break
-    if temp == 0:
-        while(stack):
-            if target != stack[-1]:
-                stack.pop()
-            else:
-                temp = 1
+
+    # 스택에서 target 찾기
+    if not found:
+        while stack:
+            if target == stack.pop():
+                found = True
                 break
-    if temp == 0:
+
+    # target이 어디에도 없을 경우
+    if not found:
         print("Sad")
         exit()
 
